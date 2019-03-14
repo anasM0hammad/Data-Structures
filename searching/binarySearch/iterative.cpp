6f8e3666dad3efd214ce8517b7800c5d609383e2
anasM0hammad <anas.1633.m@gmail.com>
2019-03-14 19:33:05 +0530

    binary search iterative method

---
+++ searching/binarySearch/iterative.cpp
@@ -0,0 +1,48 @@
+// WELCOME MOHAMMAD ANAS
+#include<iostream>
+ 
+using namespace std ;
+
+int findPos(int arr[] , int key , int size){
+    int pos = -1 ;
+    int beg = 0 ;
+    int end = size-1 ;
+
+    while(beg < end){
+        int mid = (beg+end)/2 ;
+
+        if(arr[mid] == key)
+            return mid ;
+
+        else if(arr[mid] > key)
+           end = end-1 ;
+
+        else
+          beg = beg+1 ;       
+
+    }
+
+    return pos ;
+
+}
+ 
+int main(){
+
+ int size ;
+ cout<<"Enter the size of array : " ;
+ cin>>size ;
+
+ int arr[size] ;
+
+ cout<<"Enter Element in sorted order : " ;
+ for(int i=0 ; i<size ; i++)
+  cin>>arr[i] ;
+
+ int key ;
+ cout<<"Enter Key : " ;
+ cin>>key ;
+
+ cout<<findPos(arr , key , size) ; 
+ 
+return 0 ;
+}
